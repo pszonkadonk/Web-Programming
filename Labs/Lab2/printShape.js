c9cdb613735b8f0c7c41261f813954e373d3e628
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-01-27 21:10:11 -0500

    Triangle working but very ugly

--- Labs/Lab2/printShape.js
+++ Labs/Lab2/printShape.js
@@ -11,29 +11,86 @@
 // 	}
 // };
 
+function UserException(message) {
+	this.message = message;
+	this.name = "UserException";
+}
+
 let triangle = (lines) => {
+	if(typeof(lines) !== "number") {
+		throw new UserException("Please enter lines as a number")
+	}
 	let triangleLine = "";
-	let counter = 1;
-	for(let i = 0; i < lines; i++) {
-		// first two lines
-		if(lines === 1) {
-			console.log("/\\");
-			break;
+	if(lines === 1){
+		triangleLine += "/\\";
+		console.log(triangleLine); 
+	}
+	else{
+		let outerpadding = "";
+		let innerPadding = "";
+		let shifter = 0;
+		let otherShifter = 0;
+		let fooShifter = 0;
+		for(let i = 1; i <=lines; i++) {
+			// console.log(`I: ${i}`);
+			// console.log(`lines: ${lines}`);
+			// console.log('');
+			otherShifter = otherShifter + 1;
+			fooShifter = lines - i;
+
+			if(i === lines) {
+				innerPadding = Array((lines + shifter)).join("-");
+				// outerpadding = Array((lines-2)-1).join("s");
+				triangleLine = "/" + innerPadding + "\\";
+				console.log(triangleLine);
+			}else {
+				innerPadding = Array(otherShifter).join("s");
+				outerpadding = Array((fooShifter)+1).join("s");
+				triangleLine = outerpadding + "/" + innerPadding + "\\";
+				console.log(triangleLine);
+				shifter +=1;
+				otherShifter+=1;
+			}
 		}
-		else if(lines === 2){
-			console.log(" /\\");
-			console.log("/--\\");
-			break;
-		}
+	}
+}
 
-		if(lines >= 3) {
-			console.log('foo');
+
+triangle(10);
 
 
 
 
 
-		}
+
+
+
+
+
+
+
+	// let triangleLine = "";
+	// let counter = 1;
+	// for(let i = 0; i < lines; i++) {
+	// 	// first two lines
+	// 	if(lines === 1) {
+	// 		console.log("/\\");
+	// 		break;
+	// 	}
+	// 	else if(lines === 2){
+	// 		console.log(" /\\");
+	// 		console.log("/--\\");
+	// 		break;
+	// 	}
+
+	// 	if(lines >= 3) {
+	// 		console.log('foo');
+
+
+
+
+
+	// 	}
 		// let leftPadding = Array(lines-i).join(" ");
 		// if(i !== 0) {
 		// 	let middlePadding
@@ -52,7 +109,7 @@ let triangle = (lines) => {
 		// console.log(triangleLine);
 		// counter +=1;
 		// }
-	}
+	
 
 
 
@@ -89,14 +146,12 @@ let triangle = (lines) => {
 	// 	case 5:  //10 spaces
 	// 		console.log("    /\\");   
 	// 		console.log("   /  \\"); 
-	// 		console.log("  /    \\");
+	// 		console.log("  /    \\"); 
 	// 		console.log(" /      \\"); lines + 2
 	// 		console.log("/--------\\"); //lines + 3 for horizontal
 	// 		break;
 	// }
-}
 
-triangle(3);
 
 
 
