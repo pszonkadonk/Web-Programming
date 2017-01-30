e49e9ea6b6b4a985316d9f6227a3b82fa09b3f77
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-01-29 21:37:30 -0500

    Add interface to index.js and finish

--- Labs/Lab2/index.js
+++ Labs/Lab2/index.js
@@ -1,1 +1,85 @@
-console.log("hello world");
+const printShape = require("./printShape");
+const prompt = require("prompt");
+
+
+function getInfo() {
+	prompt.start();
+
+	// Ask for the sizes of each shape
+
+	const triangleSize = {
+	name: 'triangleSize',
+    description: 'How many lines would you like the triangle to be',     // Prompt displayed to the user. If not supplied name will be used.,
+    type: 'number',                 // Specify the type of input to expect. 
+    default: 2,      		        // Default value to use if no value is entered. 
+    required: true                  // If true, value entered must be non-empty. 
+	};
+
+	const squareSize = {
+		name: 'squareSize',
+		description: 'How many lines would you like square to be',
+		type: 'number',
+		default: 2,
+		required: true
+	};
+
+	const rhombusSize = {
+		name: 'rhombusSize',
+		description: 'How many lines would you like the rhombus to be',
+		type: 'number',
+		default: 2,
+		required: true
+	};	
+
+	const quitPrompt = {
+		name: 'quit',
+		description: 'Would you like to quit after the results?  Please answer true or false',
+		type: 'boolean',
+		required: true
+	};
+
+	prompt.get([triangleSize, squareSize, rhombusSize, quitPrompt], function(err,result){
+
+		if(result) {
+			if (isNaN(result.triangleSize)) {
+                console.log("Triangle lines not a number");
+                getInfo();
+                return;
+            }
+            if (isNaN(result.squareSize)) {
+                console.log("Square lines not a number");
+                getInfo();
+                return;
+            }
+            if (isNaN(result.rhombusSize)) {
+                console.log("Rhombus lines not a number");
+                getInfo();
+                return;
+            }
+
+			let quit = result.quit;
+
+			for(let i = 1; i <= 10; i++){
+				console.log(i);
+				printShape.triangle(result.triangleSize);
+			}
+			for(let i = 1; i <= 10; i++){
+				console.log(i);
+				printShape.square(result.squareSize);
+			}
+			for(let i = 1; i <= 10; i++){
+				console.log(i);
+				printShape.rhombus(result.rhombusSize);
+			}
+			if(!quit) {
+				getInfo();
+			}
+		} else if(err) {
+			console.log(err);
+		}
+	});
+}
+
+getInfo();
+
+
--- Labs/Lab2/printShape.js
+++ Labs/Lab2/printShape.js
@@ -1,225 +1,119 @@
-// module.exports = {
-// 	description: "This is the printShape module",
-// 	triangle:(lines) => {
+module.exports = {
+	triangle: (lines) => {
+		if(typeof(lines) !== "number") {
+			throw("Please enter triangle lines as a number");
+		}
+		if(lines < 0 || lines === undefined) {
+			throw("Please enter a number of triangle lines greater then or equal to zero");
+		}
+		let triangleLine = "";
+		let outerpadding = "", innerPadding = "";
+		let baseShifter = 0, outerShifter = 0, innerShifter = 1;
 
-// 	},
-// 	square:(lines)=>{
+		for(let i = 1; i <= lines; i++) {
+			outerShifter = lines - i;
+			if(i === lines) {
+				innerPadding = Array((lines + baseShifter)).join("-");
+				triangleLine = "/" + innerPadding + "\\";
+				console.log(triangleLine);
+			}else {
+				innerPadding = Array(innerShifter).join(" ");
+				outerpadding = Array((outerShifter)+1).join(" ");
+				triangleLine = outerpadding + "/" + innerPadding + "\\";
+				console.log(triangleLine);
+				baseShifter +=1;
+				innerShifter+=2;
+			}
+		}
+	},
+	square: (lines) => {
+		if(typeof(lines) !== "number") {
+			throw("Please enter square lines as a number");
+		}
+		if(lines < 2 || lines === undefined) {
+			throw("Please enter a number of square lines greater then or equal to two");
+		}
+		let squareLine = "";
+		let verticalPadding = "";
+		let outerPadding = "";
 
-// 	},
-// 	rhombus(lines)=>{
-
-// 	}
-// };
-
-function UserException(message) {
-	this.message = message;
-	this.name = "UserException";
+		for(let i = 1; i <= lines; i++) {
+			if(i === 1 || i === lines) {
+				verticalPadding = Array(lines+1).join("-");
+				outerPadding = Array(lines+1).join(" ");
+				squareLine = outerPadding + "|" + verticalPadding + "|";
+				console.log(squareLine);
+			} else{
+				verticalPadding = Array(lines+1).join(" ");
+				outerPadding = Array(lines+1).join(" ");
+				squareLine = outerPadding + "|" + verticalPadding + "|"
+				console.log(squareLine);
+			}	
+		}
+	},
+	rhombus: (lines)=> {
+		if(typeof(lines) !== "number") {
+			throw("Please enter rhombus lines as a number");
+		}
+		if(lines < 2 || lines === undefined ) {
+			throw("Please enter a number of rhombus lines greater then or equal to 2");
+		}
+		if(lines % 2 !==  0) {
+			throw("Please enter an even number of rhombus lines");
+		}
+		
+		let rhombusLine = "", innerPadding = "", outerpadding="";
+		let outerShifter = lines+1, innerShifter = 1;
+		
+		for(let i = 1; i <= lines; i++) {
+			if(i === 1) {
+				innerPadding = "-";
+				outerpadding = Array(lines+1).join(" ");
+				rhombusLine = outerpadding + "/"+innerPadding + "\\";
+				console.log(rhombusLine);
+				outerShifter-=1;
+				innerShifter+=1;
+			}
+			else if(i === lines) {
+				outerShifter = lines +1;
+				innerPadding = "-";
+				outerpadding = Array(outerShifter).join(" ");
+				rhombusLine = outerpadding + "\\"+innerPadding + "/";
+				console.log(rhombusLine);
+			}
+			else if(i < 0.5 * lines) {
+				innerPadding = Array(innerShifter+i).join(" "); 
+				outerpadding = Array(outerShifter).join(" ");
+				rhombusLine = outerpadding +"/" + innerPadding + "\\";
+				console.log(rhombusLine);
+				outerShifter-=1;
+				innerShifter+=1;
+			}
+			else if(i === 0.5 * lines)
+			{
+				innerPadding = Array(lines).join(" "); 
+				outerpadding = Array(outerShifter).join(" ");
+				rhombusLine = outerpadding +"/" + innerPadding + "\\";
+				console.log(rhombusLine);
+				innerShifter = lines;
+			 }
+			 else if(i === (0.5 * lines+1)) {
+				innerPadding = Array(innerShifter).join(" "); 
+				outerpadding = Array(outerShifter).join(" ");
+				rhombusLine = outerpadding +"\\" + innerPadding + "/";
+				console.log(rhombusLine);
+				outerShifter+=1;
+				innerShifter-=1;
+			}
+			else if(i > 0.5 * lines) {
+				innerShifter-=1;
+				innerPadding = Array(innerShifter).join(" ");
+				outerpadding = Array(outerShifter).join(" ");
+				rhombusLine = outerpadding + "\\" + innerPadding + "/";
+				console.log(rhombusLine); 
+				outerShifter+=1;
+				innerShifter-=1;
+			}
+		}		
+	}
 }
-
-let triangle = (lines) => {
-	if(typeof(lines) !== "number") {
-		throw new UserException("Please enter lines as a number");
-	}
-	if(lines < 0 || lines === undefined) {
-		throw new UserException("Please enter a number greater then or equal to zero");
-	}
-
-	let triangleLine = "";
-	let outerpadding = "", innerPadding = "";
-	let baseShifter = 0, outerShifter = 0, innerShifter = 1;
-
-	for(let i = 1; i <= lines; i++) {
-		outerShifter = lines - i;
-		if(i === lines) {
-			innerPadding = Array((lines + baseShifter)).join("-");
-			triangleLine = "/" + innerPadding + "\\";
-			console.log(triangleLine);
-		}else {
-			innerPadding = Array(innerShifter).join(" ");
-			outerpadding = Array((outerShifter)+1).join(" ");
-			triangleLine = outerpadding + "/" + innerPadding + "\\";
-			console.log(triangleLine);
-			baseShifter +=1;
-			innerShifter+=2;
-		}
-	}
-}
-
-
-let square = (lines) => {
-	if(typeof(lines) !== "number") {
-		throw new UserException("Please enter lines as a number");
-	}
-	if(lines < 2 || lines === undefined) {
-		throw new UserException("Please enter a number greater then or equal to zero");
-	}
-
-	let squareLine = "";
-	let verticalPadding = "";
-
-	for(let i = 1; i <= lines; i++) {
-		if(i === 1 || i === lines) {
-			verticalPadding = Array(lines+1).join("-");
-			squareLine = "|" + verticalPadding + "|";
-			console.log(squareLine);
-		} else{
-			verticalPadding = Array(lines+1).join(" ");
-			squareLine = "|" + verticalPadding + "|"
-			console.log(squareLine);
-		}
-	}
-}
-
-let rhombus = (lines)=> {
-	
-	if(typeof(lines) !== "number") {
-		throw new UserException("Please enter lines as a number");
-	}
-	if(lines < 2 || lines === undefined ) {
-		throw new UserException("Please enter a number greater then or equal to 2");
-	}
-	if(lines % 2 !==  0) {
-		throw new UserException("Please enter an even number");
-	}
-	
-	let rhombusLine = "", innerPadding = "", outerpadding="";
-	let outerShifter = lines+1, innerShifter = 1;
-	
-	for(let i = 1; i <= lines; i++) {
-		if(i === 1) {
-			innerPadding = "-";
-			outerpadding = Array(lines+1).join(" ");
-			rhombusLine = outerpadding + "/"+innerPadding + "\\";
-			console.log(rhombusLine);
-			outerShifter-=1;
-			innerShifter+=1;
-		}
-		else if(i === lines) {
-			outerShifter = lines +1;
-			innerPadding = "-";
-			outerpadding = Array(outerShifter).join(" ");
-			rhombusLine = outerpadding + "\\"+innerPadding + "/";
-			console.log(rhombusLine);
-		}
-		else if(i < 0.5 * lines) {
-			innerPadding = Array(innerShifter+i).join(" "); 
-			outerpadding = Array(outerShifter).join(" ");
-			rhombusLine = outerpadding +"/" + innerPadding + "\\";
-			console.log(rhombusLine);
-			outerShifter-=1;
-			innerShifter+=1;
-		}
-		else if(i === 0.5 * lines)
-		{
-			innerPadding = Array(lines).join(" "); 
-			outerpadding = Array(outerShifter).join(" ");
-			rhombusLine = outerpadding +"/" + innerPadding + "\\";
-			console.log(rhombusLine);
-			innerShifter = lines;
-		 }
-		 else if(i === (0.5 * lines+1)) {
-			innerPadding = Array(innerShifter).join(" "); 
-			outerpadding = Array(outerShifter).join(" ");
-			rhombusLine = outerpadding +"\\" + innerPadding + "/";
-			console.log(rhombusLine);
-			outerShifter+=1;
-			innerShifter-=1;
-		}
-		else if(i > 0.5 * lines) {
-			innerShifter-=1;
-			innerPadding = Array(innerShifter).join(" ");
-			outerpadding = Array(outerShifter).join(" ");
-			rhombusLine = outerpadding + "\\" + innerPadding + "/";
-			console.log(rhombusLine); 
-			outerShifter+=1;
-			innerShifter-=1;
-		}
-	}
-			
-}
-	
-
-triangle(36);
-square(36);
-rhombus(36);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// 	let padding = Array((lines-i) + 1).join(" ");
-	// 	if(lines !== 1 && i === lines-1) {
-	// 		let basePadding = Array(lines+1).join("-");
-	// 		let triangleLine = "/" + basePadding + "\\";
-	// 		console.log(triangleLine);
-	// 	}else {
-	// 	let triangleLine = padding + "/" + padding + "\\";
-	// 	console.log(triangleLine);
-	// 	}
-	// }
-
-	// switch(lines) {
-	// 	case 1:
-	// 		console.log("/\\");
-	// 		break;
-	// 	case 2:
-	// 		console.log("/\\");
-	// 		console.log("/  \\");
-	// 		break;
-	// 	case 3:  // 3 spaces
-	// 		console.log("  /\\");    //spaces = lines-i. ie 2 spaces
-	// 		console.log(" /  \\");  // 1 space
-	// 		console.log("/    \\"); // 0 spaces
-	// 		break;
-	// 	case 4: //6 spaces
-	// 		console.log("   /\\");   
-	// 		console.log("  /  \\"); //= lines - 2
-	// 		console.log(" /    \\"); //horizonal - = lines
-	// 		console.log("/------\\"); // = lines + 2
-	// 		break;
-	// 	case 5:  //10 spaces
-	// 		console.log("    /\\");   
-	// 		console.log("   /  \\"); 
-	// 		console.log("  /    \\"); 
-	// 		console.log(" /      \\"); lines + 2
-	// 		console.log("/--------\\"); //lines + 3 for horizontal
-	// 		break;
-	// }
-
-
-
-
-
-
