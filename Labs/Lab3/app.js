97a5b614a956e3cfd3207bc3aceda9bdcc831499
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-02-07 12:00:16 -0500

    App.js

--- Labs/Lab3/app.js
+++ Labs/Lab3/app.js
@@ -1,73 +1,165 @@
 const fileData = require('./fileData');
 const textMetrics = require('./textMetrics');
+const fs = require('fs');
 
-let filePath = "C:/Users/Michael szonka/Desktop/chapter1.txt";
-let JSONPath = "./test.json";
-let fileToWrite = "./test.txt";
-let JSONFileToWrite = "./jsonFileToWrite.json"
-let text = "boogie w";
+let filePath = "./chapter";
 
-let jsonToWrite = [
-    {
-        "name": "Philip Barese",
-        "codename": "The Spy",
-        "role": "inconspicuously making your coffee while overhearing all your secret plans."
-    },
-    {
-        "name": "Jordan Rails",
-        "codename": "The Face",
-        "role": "changing the face of education -- and making sure that no one catches on."
+fs.open(filePath, "r", (err) => {
+    if(!err) {  
+        fileData.getFileAsString(filePath)
+        .then((data) => {
+            console.log(data);
+        })
+    } else {
+        let fileRegEx = /[\w]+/
+        file = fileRegEx.exec(filePath)[0];
+        fileData.getFileAsString(file.concat(".txt"))
+        .then((data) => {
+            let simplifiedText = textMetrics.simplify(data);
+            return simplifiedText;
+        }, (reason) => {
+            console.log(reason);
+        })
+        .then((simplifiedText) => {
+            let simplifiedFile = file + ".debug.txt";
+            fileData.saveStringToFile(simplifiedFile, simplifiedText);
+            return simplifiedText;
+        }, (reason) => {
+            console.log(reason);
+        })
+        .then((simplifiedText) => {
+            let textFileMetrics = textMetrics.createMetrics(simplifiedText);
+            return textFileMetrics;
+        }, (reason) => {
+            console.log(reason);
+        })
+        .then((textFileMetrics) => {
+            let metricFile = file + ".result.json";
+            fileData.saveJSONToFile(metricFile, textFileMetrics);
+            return metricFile;
+        }, (reason) => {
+            console.log(reason);
+        })
+        .then((metricFile) => {
+            let theMetrics = fileData.getFileAsString(metricFile);
+            console.log(theMetrics);
+        }, (reason) => {
+            console.log(reason);
+        })
     }
-]
+});
 
 
 
 
-let loadData = (filePath) => {
 
-    fileData.getFileAsString(filePath)
-    .then((data) => { // data fulfilled do something
-        // console.log(data);
-    }, (reason) => { //reject error, i.e file not found 
-        console.log(typeof(reason))
-        console.log(reason);
-    });
-}
 
-let loadJSONdata = (filePath) => {
+// let t = "Hello, my friends! This is a great day to say hello.\n\nHello! 2 3 4 23";
+// let foo = textMetrics.createMetrics(t);
+
+// console.log(foo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let filePath = "C:/Users/Michael szonka/Desktop/chapter1.txt";
+// let JSONPath = "./test.json";
+// let fileToWrite = "./test.txt";
+// let JSONFileToWrite = "./jsonFileToWrite.json"
+// let text = "boogie w";
+
+// let jsonToWrite = [
+//     {
+//         "name": "Philip Barese",
+//         "codename": "The Spy",
+//         "role": "inconspicuously making your coffee while overhearing all your secret plans."
+//     },
+//     {
+//         "name": "Jordan Rails",
+//         "codename": "The Face",
+//         "role": "changing the face of education -- and making sure that no one catches on."
+//     }
+// ]
+
+
+
+
+
+//  loadData = (filePath) => {
+
+//     fileData.getFileAsString(filePath)
+//     .then((data) => { // data fulfilled do something
+//         // console.log(data);
+//     }, (reason) => { //reject error, i.e file not found 
+//         console.log(typeof(reason))
+//         console.log(reason);
+//     });
+// }
+
+// let loadJSONdata = (filePath) => {
     
-    fileData.getFileAsJSON(JSONPath)
-    .then((data) =>{
-        console.log(data);
-    },(reason) => {
-        console.log(reason)
-    });
-}
+//     fileData.getFileAsJSON(JSONPath)
+//     .then((data) =>{
+//         console.log(data);
+//     },(reason) => {
+//         console.log(reason)
+//     });
+// }
 
-let writeDataToFile = (filePath, text) => {
+// let writeDataToFile = (filePath, text) => {
     
-    fileData.saveStringToFile(filePath, text)
-    .then((resolved) => {
-        if(resolved) {
-            console.log("Data has been written to file");
-        }
-    }, (reason) => {
-        console.log(reason);
-    });
-};
+//     fileData.saveStringToFile(filePath, text)
+//     .then((resolved) => {
+//         if(resolved) {
+//             console.log("Data has been written to file");
+//         }
+//     }, (reason) => {
+//         console.log(reason);
+//     });
+// };
 
-let writeJSONToFile = (filePath, JSONObject) => {
+// let writeJSONToFile = (filePath, JSONObject) => {
 
-    fileData.saveJSONToFile(filePath, JSONObject)
-    .then((resolved) => {
-        console.log("Data has been written to file");
-    }, (reason) => {
-        console.log(reason);
-    });
-}
+//     fileData.saveJSONToFile(filePath, JSONObject)
+//     .then((resolved) => {
+//         console.log("Data has been written to file");
+//     }, (reason) => {
+//         console.log(reason);
+//     });
+// }
 
-
-console.log(textMetrics.simplify("Daring Democrats to keep their word at the expense of several sleepless nights, Senator John Cornyn of Texas, the chamber’s No. 2 Republican, said the Senate session would not end until lawmakers confirmed four of Mr. Trump’s nominees: Ms. DeVos, Senator Jeff Sessions of Alabama as attorney general, Representative Tom Price of Georgia to lead the Department of Health and Human Services, and Steven T. Mnuchin as Treasury secretary."));
 
 
 // loadData(filePath);
--- Labs/Lab3/textMetrics.js
+++ Labs/Lab3/textMetrics.js
@@ -1,17 +1,55 @@
 module.exports = {
     simplify: (text) => {
         if(typeof(text) !== "string") {
-            reject(new Error("Supplied data must be a string!"));
+            throw new Error("Supplied data must be a string!");
         }
         if(text === "") {
-            reject(new Error("Text supplied cannot be an empty string!"));
+            throw new Error("Text supplied cannot be an empty string!");
         }
         let simplifiedText = text.replace(/[^\w\s]/g, "")
-                                  .replace(/[\n\t+]/g, " ")
+                                  .replace(/[\n]+/g, " ") 
+                                  .replace(/[\t]+/g, " ")
                                   .toLowerCase();
         return(simplifiedText);
     },
     createMetrics: (text) => {
+        let simplifiedText = module.exports.simplify(text);
+        let textArray = simplifiedText.split(" ");
+        let totalLetters = 0, totalWords = 0, uniqueWords = 0;
+        let longWords = 0, averageWordLength = 0;
+        let wordOccurrences = {}, metrics = {};
+
+        uniqueTextArray = textArray.filter((word, index, textArray) => {  // remove duplicates
+            let wordIndex = textArray.indexOf(word);
+            if(wordIndex !== index) {  //increment word occurence if not unique
+                wordOccurrences[word] = (wordOccurrences[word] || 0) + 1;
+            }
+            else {                      //otherwise add word to occurence dictionary
+                wordOccurrences[word] = 1;
+                return wordIndex === index;
+            }
+        });
+
+        textArray.forEach((word) => { // count total letters and words
+            totalLetters += word.length;
+            totalWords +=1;
+        });
         
+        averageWordLength = totalLetters/totalWords; // average word length
+        uniqueWords = uniqueTextArray.length;   // total unique words
+        
+        uniqueTextArray.forEach((word) => {          // count long words
+            if(word.length >= 6) {
+                longWords += 1;
+            }
+        });
+        return metrics =  {
+            "totalLetters": totalLetters,
+            "totalWords": totalWords,
+            "uniqueWords": uniqueWords,
+            "longWords": longWords,
+            "averageWordLength": averageWordLength,
+            "wordOccurrences": wordOccurrences   
+        }; 
     }
 }
