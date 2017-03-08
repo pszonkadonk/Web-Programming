e2e133ceb65aef9c2ee1b72142351afb58af48c9
Michael Pszonka <pszonkadonk@host-155-246-140-31.dhcp.stevens-tech.edu>
2017-03-08 17:11:40 -0500

    Clean app.js

--- Labs/Lab7/app.js
+++ Labs/Lab7/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const configRoutes = require('./routes');
-const rt = require('./data/comments');
 
 
 // body-parser middleware
@@ -10,15 +9,6 @@ const rt = require('./data/comments');
 app.use(bodyParser.json());
 configRoutes(app);
 
-let foo = {
-    _id: '0a5a59af-2041-4ace-98ff-efd8dac18a0b',
-    poster: 'Michael',
-    comment: 'These eggs are terrible'
- }
-
-// rt.updateComment("d1806f1c-116d-4c10-a7d8-c1b441d05fbf","4e5c8b57-dccc-4ab2-99d2-d25b05af7e89", {
-// 	"comments": "Geeeeee!"
-// });
 
 app.listen(3000, () => {
     console.log("Server has started");
