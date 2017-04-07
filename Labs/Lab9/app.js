15888c87c1b647740cbec4e75f1b3a7e1cf5cb13
pszonkadonk <mjp529@gmail.com>
2017-04-06 22:01:32 -0400

    add passport authentication to /private

--- Labs/Lab9/app.js
+++ Labs/Lab9/app.js
@@ -11,6 +11,7 @@ const static = express.static(__dirname + '/public');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt-nodejs');
+const flash = require('connect-flash');
 // const configRoutes = require('./routes'); // routes
 
 
@@ -36,9 +37,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use('/public', static);
-app.use(passport.initialize());
 app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());   
+
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -48,24 +51,23 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.use(new LocalStrategy(
-    (username, password, done) => {
+passport.use(new LocalStrategy({
+        passReqToCallback: true // don't forget this
+    },
+    (req, username, password, done) => {
         let targetUser;
         for(let user of data.userData) { //find user with username and verify password
             if(user.username === username && 
                 verifyPassword(password, user.hashedPassword)) {
                 targetUser = user;
-                console.log("user verified");
                 break;
-                
             }
         }
         if(targetUser) {
-            console.log("return targetUser")
             return done(null, targetUser)  
         }
         else {
-            return done(null, false);
+            return done(null, false, req.flash('invalid', 'Invalid username or password, please try again'));
         }
     }
 ));
@@ -77,35 +79,40 @@ app.set('view engine', 'handlebars');
 // configRoutes(app);
 
 
-app.get("/", (req,res) => {
-    console.log("hello");
-    if(true) {
+
+function ensureAuthenticated(req,res,next) {
+    if(req.user) {
+        return next();
+    }
+    else {
         res.redirect('/login');
     }
-        // res.render("user/login");
-    // if(user == "authenticated") {
-    //     res.redirect("/private");
-    // }
-    // else {
-    // }
+}
+
+app.get("/", (req,res) => {
+    if(req.session.passport) {
+        res.redirect('/private')
+    }
+    else {
+        res.redirect('/login');
+    }
 });
 
 app.get('/login', (req,res) => {
-    res.render('user/login');
+    res.render('user/login', {error: req.flash('invalid')});
 })
 
 app.post("/login", 
     passport.authenticate('local',
     { failureRedirect: '/login',
-     successRedirect: '/private'
+     successRedirect: '/private',
+     failureFlash: true
     }
 ));
 
+app.get("/private", ensureAuthenticated, (req, res) => {    
 
-app.get("/private", (req, res) => {
-    //only valid users can get to /private
-    
-    let loggedUser = req.session.passport.user
+    let loggedUser = req.user
     res.render("user/user_profile", loggedUser);
 });
 
